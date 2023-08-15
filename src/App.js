bc6ef8a915c97326c6e2298a230e2131979ac1fb
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-08-16 01:09:29 +0530

    Nav componenet added

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Nav from "./component/nav";
 
+export default function Main(){
+    return(
+        <Nav />
+        
+    )
+}
