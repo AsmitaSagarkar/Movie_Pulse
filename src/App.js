3da3e74d745df7e9c9f95e3caf9adc341c97ba25
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-08-19 00:42:59 +0530

    different routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import Nav from "./component/nav";
+import Nav from "./component/nav/nav";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from "./component/home/home";
+
 
 export default function Main(){
     return(
+        <div>
+        <Router>
         <Nav />
+        <Routes>
+        <Route index element = {<Home />}></Route>
+        <Route path="movie/:id" element={<h1>Its the id section</h1>}></Route>
+        <Route path="movies/:type" element={<h1>It is the type route</h1>}></Route>
+        <Route path="/*" element={<h1>error</h1>}></Route>
+        </Routes>
+        </Router>
+        </div>
+        
+        
         
     )
 }
