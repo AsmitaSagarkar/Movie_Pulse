c5b00f8fd99dfafe057635ecbff73d540626d173
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-08-20 00:28:16 +0530

    usestate added and data from the api is fetched

--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -1,14 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./home.css";
-export default function Home(){
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
+export default function Home() {
 
-    useEffect(()=>{
+    const [popularMovies, setPopularMovies] = useState([]);
+    useEffect(() => {
         fetch("https://api.themoviedb.org/3/movie/popular?api_key=b20d18a891030a59756d2671848d1505&language=en-US")
-        .then(res => res.json()).then(data => console.log(data.results));
-    },[])
+            .then(res => res.json()).then(data => setPopularMovies(data.results));
+    }, [])
     return (
         <>
-        <h1>hello</h1>
+            <div className="poster">
+                <Carousel
+                    showThumbs={false}
+                    autoPlay={true}
+                    transitionTime={3}
+                    infiniteLoop={true}
+                    showStatus={false}
+                    stopOnHover={true}
+                >
+                    {popularMovies.map(movie => (
+                        <Link to={`/movies/${movie.id}`}>
+                            <div className="posterimage">
+                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt="movieposter"></img>
+                            </div>
+                            <div className="detail">
+                                <div className="title">{movie ? movie.original_title : ""}</div>
+                                <div className="extradetails">{movie ? movie.release_date : ""}</div>
+                                <div className="releasedate">
+                                    {movie ? movie.vote_average : ""}<i class="fa-solid fa-star"></i>{" "}
+                                </div>
+
+                                <div className="description">{movie ? movie.overview :""}</div>
+
+
+                            </div>
+
+                        </Link>
+
+
+                    ))}
+                </Carousel>
+            </div>
         </>
     )
 
