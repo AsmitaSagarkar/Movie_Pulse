4d0a74b73229d8817d8337beb9fd1d12e37cd632
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-10-18 01:03:13 +0530

    responsive navbar created

--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -1,34 +1,50 @@
 import Logo from "./logo.png";
-import Menu from "./menu.png";
-import Close from "./close.png";
+// import Menu from "./menu.png";
+// import Close from "./close.png";
+import { AiOutlineInstagram, AiOutlineFacebook, AiOutlineTwitter } from 'react-icons/ai';
+import { CiMenuFries } from 'react-icons/ci';
+
 import { Link } from "react-router-dom";
 import "./nav.css";
 import { useState } from "react";
 
+
 export default function Nav() {
+    const [showIcons,setShowIcons] =useState(false);
+    function toggle(){
+        setShowIcons(!showIcons);
 
-    const [navBarOpen, setNavBarOpen] = useState(false);
-    const toggleNavBar = () => {
-        setNavBarOpen(!navBarOpen);
-    };
+    }
 
+    
 
     return (
         <nav className="nav">
-
-            <img src={Logo} className="image" alt="Logo" />
-
-            <div className="items">
-
-                <Link className="link" to="/">Home</Link>
-                <Link className="link" to="/movies/popular">Popular</Link>
-                <Link className="link" to="/movies/topRated">Top Rated</Link>
-                <Link className="link" to="/movies/upcoming">Upcoming</Link>
-                
+            <div className="left">
+                <img src={Logo} className="image" alt="Logo" />
             </div>
-            <div className="navbarButton" onClick={toggleNavBar}>
-                    <span className="icons menu"> {navBarOpen? Menu:Close} </span>
-                    
+
+            <div className="mid">
+                <div className="link">
+                    <Link className="tag" to="/">Home</Link>
+                    <Link className="tag" to="/movies/popular">Popular</Link>
+                    <Link className="tag" to="/movies/topRated">Top Rated</Link>
+                    <Link className="tag" to="/movies/upcoming">Upcoming</Link></div>
+
+            </div>
+
+            <div className="right">
+                <div className="social-media">
+                    <Link to="#" className="icons instagram"><AiOutlineInstagram /></Link>
+                    <Link to="#" className="icons"><AiOutlineFacebook /></Link>
+                    <Link to="#" className="icons"><AiOutlineTwitter /></Link>
+                </div>
+
+                <div className="menu">
+                    <Link to="#" onClick={toggle}><CiMenuFries className="menufries" /></Link>
+                </div>
+
+
             </div>
 
         </nav>
