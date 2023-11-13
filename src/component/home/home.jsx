5b009d56cef02c69ed7b2fb9874ae0465624c229
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-11-14 01:14:06 +0530

    css converted to tailwind css

--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import "./home.css";
+// import "./home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from 'react-router-dom';
+import "../../index.css";
 export default function Home() {
 
     const [popularMovies, setPopularMovies] = useState([]);
@@ -12,7 +13,7 @@ export default function Home() {
     }, [])
     return (
         <>
-            <div className="poster">
+            <div className="relative">
                 <Carousel
                     showThumbs={false}
                     autoPlay={true}
@@ -23,17 +24,17 @@ export default function Home() {
                 >
                     {popularMovies.map(movie => (
                         <Link to={`/movies/${movie.id}`}>
-                            <div className="posterimage">
-                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt="movieposter"></img>
+                            <div className="w-full mt-12 ">
+                                <img className="md:h-screen h-96 opacity-50" src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt="movieposter"></img>
                             </div>
-                            <div className="detail">
-                                <div className="title">{movie ? movie.original_title : ""}</div>
-                                <div className="releasedate">{movie ? movie.release_date : ""}</div>
-                                <div className="ratings">
-                                    {movie ? movie.vote_average : ""}<i class="fa-solid fa-star"></i>{" "}
+                            <div className="md:absolute md:bottom-24 flex flex-col text-left  md:ml-14 md:mr-14 text-amber-50 mb-12 md:mb-0 mt-12">
+                                <div className="md:text-5xl text-2xl font-bold">{movie ? movie.original_title : ""}</div>
+                                <div className="md:text-2xl text-lg  mt-2 ">{movie ? movie.release_date : ""}</div>
+                                <div className="md:text-2xl text-lg  mt-2">
+                                    {movie ? movie.vote_average : " "}/10
                                 </div>
 
-                                <div className="description">{movie ? movie.overview :""}</div>
+                                <div className="md:text-2xl text-lg italic mt-2">{movie ? movie.overview :""}</div>
 
 
                             </div>
