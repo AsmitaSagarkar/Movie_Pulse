a2fd68ebd84dcdfe9bf426054d335e1efe0a6290
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-11-15 01:03:21 +0530

    rendered movie component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from "./component/home/home";
 // import "../public/style.css";
 import "./index.css";
+import Movie from "./component/movie/movie";
 
 export default function Main(){
     return(
@@ -14,7 +15,7 @@ export default function Main(){
         <Routes>
         <Route index element = {<Home />}></Route>
         <Route path="movie/:id" element={<h1>Its the id section</h1>}></Route>
-        <Route path="movies/:type" element={<h1>It is the type route</h1>}></Route>
+        <Route path="movies/:type" element={<Movie />}></Route>
         <Route path="/*" element={<h1>error</h1>}></Route>
         </Routes>
         </Router>
