f6400192e720b908a0fd0954fd3e14f750a4e2b2
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-11-17 01:14:18 +0530

    warnings removed and text converted to uppercase

--- src/component/movie/movie.jsx
+++ src/component/movie/movie.jsx
@@ -5,21 +5,22 @@ import { useParams } from "react-router-dom";
 const Movie = () => {
     const [movie, setMovie] = useState([]);
     const { type } = useParams();
-
-    useEffect(() => {
-        getData()
-    }, [type])
-
-    const getData = () => {
+    function getData()  {
         fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=b20d18a891030a59756d2671848d1505&language=en-US`)
             .then(res => res.json()).then(data => setMovie(data.results));
     }
+    useEffect(() => {
+        getData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [type]);
+
+    
 
     return (
         <>
 
             <div>
-                <h2>{(type ? type : "POPULAR").toUpperCase}</h2>
+            <h2 className="text-white">{(type ? type : "POPULAR").toUpperCase().replace(/_/g,' ')}</h2>
                 <div>{movie.map(movie=>
                     (
                         <Card movie={movie}/>
