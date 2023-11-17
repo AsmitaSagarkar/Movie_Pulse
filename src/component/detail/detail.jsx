bfa75c24bf174d1c42c43b374b2e44085bcc6c77
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-11-18 01:05:44 +0530

    css added

---
+++ src/component/detail/detail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../index.css"
+const Detail =() =>
+{
+return(
+<>
+<h1 className="text-neutral-100">I m a mOvie page</h1>
+</>
+)
+}
+
+export default Detail; 
