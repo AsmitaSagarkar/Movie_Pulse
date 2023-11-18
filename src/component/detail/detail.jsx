d20ac01960423f9c2d01da936cd72a3bf2ed5862
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-11-19 00:31:45 +0530

    link updated

--- src/component/detail/detail.jsx
+++ src/component/detail/detail.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import "../../index.css"
-const Detail =() =>
-{
-return(
-<>
-<h1 className="text-neutral-100">I m a mOvie page</h1>
-</>
-)
-}
+import React, { useEffect, useState } from "react";
+import "../../index.css";
+import { useParams } from "react-router-dom";
 
-export default Detail; 
+
+
+export default function Detail(){
+    const [movieDetail,setMovieDetail] = useState()
+    const { id } = useParams()
+
+    
+
+    
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=b20d18a891030a59756d2671848d1505&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieDetail(data))
+        // eslint-disable-next-line 
+    }, [])
+
+
+    return<>
+    <h2 className="text-neutral-50">{movieDetail?movieDetail.original_title:""}</h2>
+    </>
+
+}
