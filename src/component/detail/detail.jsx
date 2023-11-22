97357c7da4e1793582d7e89219360e9ccef050af
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-11-23 00:08:05 +0530

    detail page updated

--- src/component/detail/detail.jsx
+++ src/component/detail/detail.jsx
@@ -21,25 +21,52 @@ export default function Detail() {
 
     return <>
         <div>
-            <h2 className="text-neutral-50">{movieDetail ? movieDetail.original_title : ""}</h2>
-            <div>
-                <img className="h-64 w-72" src={`https://image.tmdb.org/t/p/original${movieDetail ? movieDetail.poster_path : ""}`} alt='loading'></img>
-            </div>
-            <div>
-                <Link className="text-neutral-100" to={movieDetail ? movieDetail.homepage : ""}>Watch Now</Link>
-            </div>
-            <div>
-                 <div className="text-neutral-100">{movieDetail ? movieDetail.overview : ""}</div>
-                 <div className="text-neutral-100">{movieDetail ? movieDetail.release_date : ""}</div>
-                 <div className="text-neutral-100">{movieDetail ? movieDetail.budget : ""}</div>
-                 <div className="text-neutral-100">{(movieDetail ? movieDetail.vote_average : "").toString().slice(0,3)}/10</div>
-            </div>   
+            <div className="grid grid-cols-2 mt-10">
+                <div className="flex justify-center">
+                    <img className="h-4/6 w-4/6 " src={`https://image.tmdb.org/t/p/original${movieDetail ? movieDetail.poster_path : ""}`} alt='loading'></img>
+                </div>
 
-            <div className="text-neutral-100">
+                <div className="grid h-96">
+                    <h2 className="text-neutral-100 text-5xl font-bold">{movieDetail ? movieDetail.original_title : ""}</h2>
+                    
+                    <div className="text-neutral-100 grid grid-cols-3">
 
-            console.log(movieDetail.genres);
+                        {
+                            movieDetail && movieDetail.genres
+                                ?
+                                movieDetail.genres.map(genre => (
+                                    <span className="text-neutral-100 bg-slate-900  p-1 rounded-xl">{genre.name}</span>
+
+                                ))
+                                :
+                                ""
+                        }
+                        
+
+                    </div>
+                    <div className="text-neutral-100 text-2xl">"{movieDetail?movieDetail.tagline:""}"</div>
+
+                    
+
+                    <div>
+
+                        <div className="text-neutral-100">{movieDetail ? movieDetail.release_date : ""}</div>
+                        <div className="text-neutral-100">{movieDetail ? movieDetail.budget : ""}</div>
+                        <div className="text-neutral-100">{(movieDetail ? movieDetail.vote_average : "").toString().slice(0, 3)}/10</div>
+                        <div className="text-neutral-100">{movieDetail ? movieDetail.overview : ""}</div>
+                    </div>
             
+
+            <div>
+                        <Link className="text-neutral-100" to={movieDetail ? movieDetail.homepage : ""}>Watch Now</Link>
+                    </div>
+
+                </div>
             </div>
+
+            
+
+
 
         </div>
     </>
