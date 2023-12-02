3c79dbefafa2d501d1a51cca26d704f672b7e4d0
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-12-03 00:19:48 +0530

    css updated

--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -1,11 +1,9 @@
-import Logo from "./logo.png";
-
 import { AiOutlineInstagram, AiOutlineFacebook, AiOutlineTwitter } from 'react-icons/ai';
 import { CiMenuFries } from 'react-icons/ci';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import CloseIcon from '@mui/icons-material/Close';
-
+import LiveTvIcon from '@mui/icons-material/LiveTv';
 import { Link } from "react-router-dom";
 // import "./nav.css";
 import "../../index.css";
@@ -35,17 +33,17 @@ export default function Nav() {
 
     return (
 
-        <nav className="grid md:justify-between md:grid-cols-3 mt-7 dark:bg-slate-900">
-            <div className="md:flex md:justify-center">
-                <img src={Logo} className="image" alt="Logo" />
+        <nav className="grid md:justify-between md:grid-cols-3 md:mt-7 dark:bg-slate-900 gap-20">
+            <div className="md:flex md:justify-center md:text-2xl font-fontfam font-bold md:mr-5">
+                 <h1 className=" text-white border-4 border-white pb-2 pl-2 pr-2 rounded-tl-2xl bg-nav rounded-br-2xl md:hover:bg-white md:hover:text-nav hover:cursor-pointer md:hover:border-nav">Movie Pulse <span className="text-4xl"><LiveTvIcon /></span></h1>
             </div>
 
-            <div className="md:flex md:justify-around md:items-center md:static absolute right-3 top-24 z-10 ">
+            <div className="md:flex md:justify-around md:items-center md:static absolute right-3 top-24 z-10 gap-3 ml-5 mr-5">
 
-                <div><Link to="/" className="text-nav md:text-2xl font-fontfam font-bold">Home</Link></div>
-                <div><Link className="text-nav md:text-2xl font-fontfam font-bold" to="/movies/popular">Popular</Link></div>
-                <div><Link className="text-nav md:text-2xl font-fontfam font-bold" to="/movies/top_rated">Top Rated</Link></div>
-                <div><Link className="text-nav md:text-2xl font-fontfam font-bold" to="/movies/upcoming">Upcoming</Link></div>
+                <div><Link to="/" className="text-nav md:text-2xl font-fontfam font-bold sm:text-xl">Home</Link></div>
+                <div><Link className="text-nav md:text-2xl font-fontfam font-bold sm:text-xl" to="/movies/popular">Popular</Link></div>
+                <div><Link className="text-nav md:text-2xl font-fontfam font-bold sm:text-xl" to="/movies/top_rated">Trending</Link></div>
+                <div><Link className="text-nav md:text-2xl font-fontfam sm:text-xl font-bold" to="/movies/upcoming">Upcoming</Link></div>
             </div>
 
 
@@ -57,9 +55,9 @@ export default function Nav() {
                     <div><Link to="#" className="text-nav md:text-3xl font-fontfam font-bold"><AiOutlineTwitter /></Link></div>
 
                     <Link><div className="text-nav md:text-3xl font-fontfam font-bold flex items-center">{showIcons ? 
-                        <DarkModeIcon onClick={() => { handleToggle(); addDark(); }}  />
-                       : 
                         <LightModeIcon onClick={() => { handleToggle(); removeDark(); }}  />
+                        : 
+                        <DarkModeIcon onClick={() => { handleToggle(); addDark(); }}  />
                       }
                 </div></Link>
                     
@@ -68,9 +66,10 @@ export default function Nav() {
                     
 
 
-                <div className="md:hidden absolute right-3 top-8 z-10">
+                <div className="md:hidden absolute right-3 top-4 z-10">
                     <Link to="#" onClick={handleToggle}>
-                    <div className="text-blue-500 text-3xl">{isToggle?<CloseIcon onClick={toggle}/> : <CiMenuFries onClick={toggle}/>}</div></Link>
+                    <div className="text-white text-3xl">{isToggle?<CloseIcon 
+                        onClick={toggle} className="mb-5 text-3xl"/> : <CiMenuFries onClick={toggle}/>}</div></Link>
                 </div>
 
 
