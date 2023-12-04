c3954e9cdad058f2d83239fcea4809c5f39e0595
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-12-04 23:29:33 +0530

    nav part updated

--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -3,9 +3,9 @@ import { CiMenuFries } from 'react-icons/ci';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import CloseIcon from '@mui/icons-material/Close';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
+import Pulse from "./pulse (1).png";
 import { Link } from "react-router-dom";
-// import "./nav.css";
+
 import "../../index.css";
 import { useState } from "react";
 
@@ -35,7 +35,7 @@ export default function Nav() {
 
         <nav className="grid md:justify-between md:grid-cols-3 md:mt-7 dark:bg-slate-900 gap-20">
             <div className="md:flex md:justify-center md:text-2xl font-fontfam font-bold md:mr-5">
-                 <h1 className=" text-white border-4 border-white pb-2 pl-2 pr-2 rounded-tl-2xl bg-nav rounded-br-2xl md:hover:bg-white md:hover:text-nav hover:cursor-pointer md:hover:border-nav">Movie Pulse <span className="text-4xl"><LiveTvIcon /></span></h1>
+                <img className="w-24 h-24" src={Pulse} alt='loading'/>
             </div>
 
             <div className="md:flex md:justify-around md:items-center md:static absolute right-3 top-24 z-10 gap-3 ml-5 mr-5">
