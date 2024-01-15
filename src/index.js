6c1c79ca445f7723fe766d40b02df9555840ce1b
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-16 00:33:52 +0530

    authentication added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from "react";
 import ReactDom from "react-dom";
 import Main from "./App";
 import "./index.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 // import '../public/style.css';
 
 ReactDom.render(
-    <Main />
+    <Auth0Provider
+    domain="dev-e42l55i1jjau2q6a.us.auth0.com"
+    clientId="4zRtOP2k2NFZwgssj2zJaTnki5isOV5m"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+  <Main />
+  </Auth0Provider>
+
+
+
+    
     ,document.getElementById('root'));
