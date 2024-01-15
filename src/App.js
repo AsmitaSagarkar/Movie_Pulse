1db921e991bd26d6c8c713301e8f9e1e95cc0cb1
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-16 00:33:41 +0530

    authentication added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import Home from "./component/home/home";
 import "./index.css";
 import Movie from "./component/movie/movie";
 import Detail from "./component/detail/detail";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Main(){
+    const { user ,isAuthenticated } = useAuth0();
     return(
         <div style={{ backgroundImage: 'url("https://www.transparenttextures.com/patterns/cubes.png")' }} className="dark:bg-darkBg" >
+        
+
+
 
         
         <Router>
         <Nav />
+        <div>
+        
+        {
+            isAuthenticated ? 
+            <div className="text-xl md:text-2xl mt-[20px] text-nav dark:text-cyan-400 text-center font-bold">Welcome to Movie Pulse {user.name}! Get comfy, grab your virtual popcorn, and let the movie magic begin. Enjoy the show!"</div>:
+            <div className="text-xl font-bold md:text-2xl ml-10 mt-[20px] text-nav dark:text-cyan-400 ">Welcome to Movie Pulse !</div>
+        }
+        </div>
         <Routes>
         <Route path="/*" element = {<Home />}></Route>
         <Route path="movie/:id" element={<Detail />}></Route>
@@ -21,6 +33,7 @@ export default function Main(){
         
         </Routes>
         </Router>
+        
         </div>
         
         
