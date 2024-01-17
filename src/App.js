7d70bf337dfce2de1ce1dbd0efe74baef9f805d5
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-18 01:10:22 +0530

    css updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function Main(){
     
     return(
-        <div style={{ backgroundImage: 'url("https://www.transparenttextures.com/patterns/cubes.png")' }} className="dark:bg-darkBg" >
+        <div  className="dark:bg-darkBg" >
         
 
 
