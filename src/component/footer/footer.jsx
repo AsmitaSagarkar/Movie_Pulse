b3b4fb8f6536674f332ea6c8baf54d7469d1d97b
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-19 01:00:19 +0530

    footer component added

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -5,9 +5,9 @@ export default function Footer(){
     return (
 
         <>
-        <div className="border-2 border-nav dark:border-white flex flex-row p-4">
-        <h2 className="text-nav dark:text-white md:text-2xl text-lg">Scroll, Watch, Repeat!</h2>
-        <div className="flex flex-col justify-center gap-4">
+        <div className="shandow-nav shadow-lg dark:shadow-white border-t-2 border-nav dark:border-white flex flex-col p-4">
+        <h2 className="text-nav dark:text-white md:text-2xl text-lg text-center">Scroll, Watch, Repeat!</h2>
+        <div className="flex flex-row justify-center gap-4">
         <div className='text-4xl hover:cursor-pointer'><AiOutlineInstagram /></div>
         <div className='text-4xl hover:cursor-pointer'><AiOutlineFacebook /></div>
         <div className='text-4xl hover:cursor-pointer'><AiOutlineTwitter /></div>
