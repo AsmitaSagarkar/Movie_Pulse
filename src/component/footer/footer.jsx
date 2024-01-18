1785e225565aae2bea719fb3267f341132e92b75
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-19 00:35:40 +0530

    footer component added

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+
+        <>
+        <div className='text-4xl hover:cursor-pointer'><AiOutlineInstagram /></div>
+        <div className='text-4xl hover:cursor-pointer'><AiOutlineFacebook /></div>
+        <div className='text-4xl hover:cursor-pointer'><AiOutlineTwitter /></div>
+        </>
+    )
+
+}
+
