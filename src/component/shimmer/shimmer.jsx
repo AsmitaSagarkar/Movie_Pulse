a7462441f0f077855912ca16559d90e7b2c24ec5
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-19 00:35:19 +0530

    shimmer component added

---
+++ src/component/shimmer/shimmer.jsx
@@ -0,0 +1,10 @@
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
+
+export default function Shimmer(){
+    return (
+
+        <>
+        <ShimmerSimpleGallery card imageHeight={300} />;
+        </>
+    )
+}
