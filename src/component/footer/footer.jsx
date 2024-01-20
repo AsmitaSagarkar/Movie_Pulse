a44659c2f0cc3f35b145124c51d08e1eff5772db
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-20 16:31:29 +0530

    css updated

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -6,8 +6,8 @@ export default function Footer(){
 
         <>
         <div className=" border-t-2 border-emerald-700 dark:border-white flex flex-col p-4 font-semibold mt-5  shadow-shade">
-        <h2 className="bg-gradient-to-r from-lime-500 via-green-500 to-emerald-700 bg-clip-text text-transparent md:text-2xl text-lg text-center font-extrabold">Scroll, Watch, Repeat!</h2>
-        <div className="flex flex-row justify-center gap-4 text-emerald-700 ">
+        <h2 className="bg-gradient-to-r from-lime-500 via-green-500 to-emerald-700 bg-clip-text text-transparent md:text-2xl text-lg text-center font-extrabold dark:text-cyan-400">Scroll, Watch, Repeat!</h2>
+        <div className="flex flex-row justify-center gap-4 text-emerald-700 dark:text-white">
         <div className='text-4xl hover:cursor-pointer'><AiOutlineInstagram /></div>
         <div className='text-4xl hover:cursor-pointer'><AiOutlineFacebook /></div>
         <div className='text-4xl hover:cursor-pointer'><AiOutlineTwitter /></div>
