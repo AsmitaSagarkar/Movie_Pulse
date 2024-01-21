582132e83df9122241874d5197a55f0ac132b3c3
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-21 16:58:30 +0530

    search bar completed

--- src/component/movie/movie.jsx
+++ src/component/movie/movie.jsx
@@ -32,7 +32,7 @@ const Movie = () => {
 
             
             <Nav />
-            <div>
+            <div className="mt-28">
             <h2 className="bg-gradient-to-r from-lime-500 via-green-500 to-emerald-700 bg-clip-text text-transparent text-2xl font-bold  md:ml-12 m-0 ml-4 mb-5 mt-5">WELCOME TO THE {(type ? type : "POPULAR").toUpperCase().replace(/_/g,' ')} HUB! DIVE IN.</h2>
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-12 md:ml-12 md:mr-12 gap-3 mr-2 ml-2">
                 
